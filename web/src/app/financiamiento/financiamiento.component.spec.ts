a6011121ab20e6e746f2034605399209f6d4cbd7
Ruben <ruben.msalles@gmail.com>
2019-09-02 19:56:57 -0400

    agregadas preguntas frecuentes a apoyo, desarrollo y financiamiento, con formato. falta arreglar colores de los botones. a mediad que vaya recibiendo material, delegare trabajo

--- web/src/app/financiamiento/financiamiento.component.spec.ts
+++ web/src/app/financiamiento/financiamiento.component.spec.ts
@@ -1,6 +1,6 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { FinanciamientoComponent } from './financiamiento.component';
+import {FinanciamientoComponent} from './financiamiento.component';
 
 describe('FinanciamientoComponent', () => {
   let component: FinanciamientoComponent;
@@ -8,9 +8,9 @@ describe('FinanciamientoComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ FinanciamientoComponent ]
+      declarations: [FinanciamientoComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {

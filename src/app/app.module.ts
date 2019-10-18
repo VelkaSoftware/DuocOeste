48cc643ad09f99c8cdddd8c7ceea9404d15c112d
Alex <al.magna@alumnos.duoc.cl>
2019-10-18 00:26:09 -0300

    Se agregaron 3 modulos necesarios para crear los paneles que se crearon de prueba en el componente de asuntosestudiantiles (Moveré esos paneles cuando el componente de convenios sea creado y existan botones para redireccionar)
    https://material.angular.io/guide/getting-started la guia inicial del modulo que fue importado en la aplicacion (Angular/Material)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { TopBarComponent } from './top-bar/top-bar.component';
-import { FinanciamientoComponent } from './financiamiento/financiamiento.component';
-import { AsuntosestudiantilesComponent } from './asuntosestudiantiles/asuntosestudiantiles.component';
-import { PastoralComponent } from './pastoral/pastoral.component';
-import { DesarrolloComponent } from './desarrollo/desarrollo.component';
-import { ApoyoComponent } from './apoyo/apoyo.component';
-import { DeportesComponent } from './deportes/deportes.component';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/environment';
-
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {TopBarComponent} from './top-bar/top-bar.component';
+import {FinanciamientoComponent} from './financiamiento/financiamiento.component';
+import {AsuntosestudiantilesComponent} from './asuntosestudiantiles/asuntosestudiantiles.component';
+import {PastoralComponent} from './pastoral/pastoral.component';
+import {DesarrolloComponent} from './desarrollo/desarrollo.component';
+import {ApoyoComponent} from './apoyo/apoyo.component';
+import {DeportesComponent} from './deportes/deportes.component';
+import {ServiceWorkerModule} from '@angular/service-worker';
+import {environment} from '../environments/environment';
+import {MatExpansionModule} from '@angular/material';
+import {MatInputModule} from '@angular/material'
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -29,11 +32,18 @@ import { environment } from '../environments/environment';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+    FormsModule,
+    ReactiveFormsModule,
+    MatExpansionModule, MatExpansionModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {enabled: environment.production})
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {
 }
+
+
+

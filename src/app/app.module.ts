61d538abd73f951bbd115dc14ca02356a009f3bd
Beelzebu <jsuarez.lara@outlook.com>
2019-10-24 15:24:00 -0300

    Revert "Se agregaron 3 modulos necesarios para crear los paneles que se crearon de prueba en el componente de asuntosestudiantiles (Moveré esos paneles cuando el componente de convenios sea creado y existan botones para redireccionar)"
    This reverts commit 48cc643a

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,19 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {DashboardComponent} from './dashboard/dashboard.component';
-import {TopBarComponent} from './top-bar/top-bar.component';
-import {FinanciamientoComponent} from './financiamiento/financiamiento.component';
-import {AsuntosestudiantilesComponent} from './asuntosestudiantiles/asuntosestudiantiles.component';
-import {PastoralComponent} from './pastoral/pastoral.component';
-import {DesarrolloComponent} from './desarrollo/desarrollo.component';
-import {ApoyoComponent} from './apoyo/apoyo.component';
-import {DeportesComponent} from './deportes/deportes.component';
-import {ServiceWorkerModule} from '@angular/service-worker';
-import {environment} from '../environments/environment';
-import {MatExpansionModule} from '@angular/material';
-import {MatInputModule} from '@angular/material'
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { FinanciamientoComponent } from './financiamiento/financiamiento.component';
+import { AsuntosestudiantilesComponent } from './asuntosestudiantiles/asuntosestudiantiles.component';
+import { PastoralComponent } from './pastoral/pastoral.component';
+import { DesarrolloComponent } from './desarrollo/desarrollo.component';
+import { ApoyoComponent } from './apoyo/apoyo.component';
+import { DeportesComponent } from './deportes/deportes.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+
 
 @NgModule({
   declarations: [
@@ -32,18 +29,11 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule,
     AppRoutingModule,
-    FormsModule,
-    ReactiveFormsModule,
-    MatExpansionModule, MatExpansionModule,
-    ServiceWorkerModule.register('ngsw-worker.js', {enabled: environment.production})
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {
 }
-
-
-

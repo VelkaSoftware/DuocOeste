aa218f88dd7eac65e02bf5a389248afaaf766152
Alex <al.magna@alumnos.duoc.cl>
2020-01-16 13:13:47 -0300

    -Se eliminaron los botones de asuntos estudiantiles -Se añadieron paneles expandibles que reemplazan la función de hipervínculo que poseian los anteriores botones en asuntos estudiantiles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { TopBarComponent } from './top-bar/top-bar.component';
-import { FinanciamientoComponent } from './financiamiento/financiamiento.component';
-import { AsuntosestudiantilesComponent } from './asuntosestudiantiles/asuntosestudiantiles.component';
-import { PastoralComponent } from './pastoral/pastoral.component';
-import { DesarrolloComponent } from './desarrollo/desarrollo.component';
-import { ApoyoComponent } from './apoyo/apoyo.component';
-import { DeportesComponent } from './deportes/deportes.component';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/environment';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {TopBarComponent} from './top-bar/top-bar.component';
+import {FinanciamientoComponent} from './financiamiento/financiamiento.component';
+import {AsuntosestudiantilesComponent} from './asuntosestudiantiles/asuntosestudiantiles.component';
+import {PastoralComponent} from './pastoral/pastoral.component';
+import {DesarrolloComponent} from './desarrollo/desarrollo.component';
+import {ApoyoComponent} from './apoyo/apoyo.component';
+import {DeportesComponent} from './deportes/deportes.component';
+import {ServiceWorkerModule} from '@angular/service-worker';
+import {environment} from '../environments/environment';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatExpansionModule, MatFormFieldModule} from "@angular/material";
 
 
 @NgModule({
@@ -30,10 +32,15 @@ import { environment } from '../environments/environment';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+    ServiceWorkerModule.register('ngsw-worker.js', {enabled: environment.production}),
+    BrowserAnimationsModule,
+    MatExpansionModule,
+    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {
 }
+
+

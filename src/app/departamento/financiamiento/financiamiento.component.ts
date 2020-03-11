831d64e15519112258b8d44330cd21b42ad28605
Beelzebu <jsuarez.lara@outlook.com>
2020-03-10 22:33:50 -0300

    Eliminada configuración de service worker y cosas relacionadas a la PWA, eliminado parte del código repetitivo de las tablas html en financiamiento

--- src/app/departamento/financiamiento/financiamiento.component.ts
+++ src/app/departamento/financiamiento/financiamiento.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {Router} from "@angular/router";
+import {Tabla} from "./tabla";
 
 @Component({
   selector: 'app-financiamiento',
@@ -8,13 +9,41 @@ import {Router} from "@angular/router";
 })
 export class FinanciamientoComponent implements OnInit {
 
-  constructor(private router: Router) {
+  tablas: Map<string, Tabla[]> = new Map<string, Tabla[]>();
+
+  constructor() {
+    let becasDuoc: Tabla[] = [
+      new Tabla(
+        "Beca Segundo Hermano",
+        "20",
+        "Alumno que tenga un segundo hermano matriculado en alguna carrera de Duoc UC",
+        "Presentar el certificado de nacimiento del alumno y el de su hermano(s) matriculado(s) en otra carrera de Duoc UC."),
+      new Tabla(
+        "Beca Tercer Hermano",
+        "50",
+        "Alumno que tenga un tercer hermano matriculado en alguna carrera de Duoc UC",
+        "Presentar el certificado de nacimiento del alumno y el de su hermano(s) matriculado(s) en otra carrera de Duoc UC."),
+      new Tabla(
+        "Becas deportivas",
+        "20 - 40",
+        "Esta beca beneficiará con un 20% en su arancel a alumnos deportistas que representen a la institución en alguna de las ramas de selección Duoc UC o que pertenezcan a alguna federación deportiva de nuestro país. En tanto, si el estudiante participa en ambas instancias, el beneficio es de 40%.",
+        "Los alumnos deportistas deben acercarse al Coordinador de Deportes de la sede o al entrenador de la selección a la que pertenecen para consultar respecto a la documentación necesaria u otros requisitos. Para postular, se debe acceder a http://www.duoc.cl/becasdeportiva y llenar el formulario. La asignación de esta beca está condicionada al cumplimiento de requisitos mínimos de notas y asistencia."),
+      new Tabla(
+        "BECA LICEO POLITÉCNICO ANDES",
+        "30",
+        "Destinada a egresados del Liceo Politécnico Andes",
+        "Ser egresado del Liceo Politécnico Andes"
+      ),
+      new Tabla(
+        "BECA MADRE O PADRE SOSTENEDOR",
+        "100% del arancel semestral, excluida la matrícula",
+        "Se otorga por el fallecimiento de Padre sostenedor o Madre sostenedora, válida por período de que dura la carrera y no es retroactiva",
+        "Presentar el certificado de defunción respectivo"
+      )
+    ];
+    this.tablas.set("Becas Duoc UC", becasDuoc);
   }
 
   ngOnInit() {
-  }
-
-  becasInternas() {
-    this.router.navigate([this.router.url.split("#")[0]], {fragment: 'becas-duoc'});
   }
 }
---
+++ src/app/departamento/financiamiento/tabla.ts
@@ -0,0 +1,14 @@
+export class Tabla {
+
+  beca: string;
+  beneficio: string;
+  descripcion: string;
+  requisitos: string;
+
+  constructor(beca: string, beneficio: string, descripcion: string, requisitos: string) {
+    this.beca = beca;
+    this.beneficio = beneficio;
+    this.descripcion = descripcion;
+    this.requisitos = requisitos;
+  }
+}

06c446c68d4dfddcec0268bfe88bbb909aa10bc4
Beelzebu <jsuarez.lara@outlook.com>
2020-03-16 15:17:47 -0300

    Corregido error que no permitía iniciar el entorno de pruebas de angular

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,18 @@ import {environment} from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
-}
-
-let onDeviceReady = () => {
+  let head = document.getElementsByTagName("head")[0];
+  // el siguiente script es el de cordova, utilizado únicamente en producción
+  let cordovaScript = document.createElement("script");
+  cordovaScript.type = 'text/javascript';
+  cordovaScript.src = 'cordova.js';
+  head.appendChild(cordovaScript);
+  let onDeviceReady = () => {
+    platformBrowserDynamic().bootstrapModule(AppModule)
+      .catch(err => alert(err));
+  };
+  document.addEventListener('deviceready', onDeviceReady, false);
+} else {
   platformBrowserDynamic().bootstrapModule(AppModule)
     .catch(err => alert(err));
-};
-document.addEventListener('deviceready', onDeviceReady, false);
+}
